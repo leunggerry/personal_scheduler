037ed1421cae6df7a34d6e4e98b0de284f2e8a1d
gigi.upNorth <leung.gerry@gmail.com>
2022-07-05 22:51:04 -0400

    [FE] Adding Date to header to show current date

--- res/js/script.js
+++ res/js/script.js
@@ -3,8 +3,45 @@
 
 /** Function Definitions
  ******************************************************************************/
-/** Utility Functions
+var printTodaysDate = function () {
+  var currentDayEl = $("#currentDay");
+
+  //console.log(currentDayEl);
+  currentDayEl.text(getCurrentDay());
+};
+/** Utility function
  ******************************************************************************/
+var getCurrentDay = function () {
+  var now = moment();
+  //   var day = now._d.getDate();
+  //   var month = now._d.getMonth();
+  //   var year = now._d.getFullYear();
+  //   console.log(now);
+  //   console.log(day);
+  //   console.log(month);
+  //   console.log(year);
+
+  //console.log(now.format("dddd, MMMM Do YYYY"));
+  return now.format("dddd, MMMM Do YYYY");
+};
+
+var getTheHour = function () {
+  //console.log(now.format("h"));
+
+  return moment().format("h");
+};
+
+var getTheMinute = function () {
+  //console.log(now.format("h"));
+
+  return moment().format("mm");
+};
+
+var getTheTime = function () {
+  //console.log(moment().format("h:mm a"));
+  return moment().format("h:mm a");
+};
 
 /** Main Program
  ******************************************************************************/
+printTodaysDate();
